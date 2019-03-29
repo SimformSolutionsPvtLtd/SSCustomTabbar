62768a33469fe943bad87cfbfb0dcc1400b05ec0
Sumit Goswami <sumit.g@simformsolutions.com>
2019-03-29 11:35:58 +0530

    - completed tabbar with animation

---
+++ SSCustomTabBar/SSCustomTabBar/Tabbar/UIView+Extenstion.swift
@@ -0,0 +1,18 @@
+//
+//  UIView+Extenstion.swift
+//  SSCustomTabBar
+//
+//  Created by Sumit Goswami on 27/03/19.
+//  Copyright © 2019 SimformSolutions. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func viewCenter(usePresentationLayerIfPossible: Bool) -> CGPoint {
+        if usePresentationLayerIfPossible, let presentationLayer = layer.presentation() {
+            return presentationLayer.position
+        }
+        return center
+    }
+}
